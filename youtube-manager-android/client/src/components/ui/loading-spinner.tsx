7c4bae8aebcf199351252fcd5dfc48baa5e14993
sajid19101 <sajidus191@gmail.com>
2025-05-16 04:57:07 +0200

    unzipgit

---
+++ youtube-manager-android/client/src/components/ui/loading-spinner.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { cn } from '../../lib/utils';
+
+interface LoadingSpinnerProps {
+  size?: 'sm' | 'md' | 'lg';
+  className?: string;
+}
+
+export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ 
+  size = 'md',
+  className
+}) => {
+  const sizeClasses = {
+    sm: 'h-4 w-4 border-2',
+    md: 'h-6 w-6 border-2',
+    lg: 'h-10 w-10 border-3'
+  };
+
+  return (
+    <div className={cn(
+      'animate-spin rounded-full border-solid border-primary border-t-transparent',
+      sizeClasses[size],
+      className
+    )} />
+  );
+};
